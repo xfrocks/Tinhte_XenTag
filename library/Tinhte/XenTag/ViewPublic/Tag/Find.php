9b2a3040e705bed97c7713f8e65e24fd96535b03
Dao Hoang Son <daohoangson@gmail.com>
2013-09-09 14:59:03 +0700

    fixed bug empty avatar for XenForo 1.2

--- library/Tinhte/XenTag/ViewPublic/Tag/Find.php
+++ library/Tinhte/XenTag/ViewPublic/Tag/Find.php
@@ -7,10 +7,22 @@ class Tinhte_XenTag_ViewPublic_Tag_Find extends XenForo_ViewPublic_Base
 		$results = array();
 		foreach ($this->_params['tags'] AS $tag)
 		{
-			$results[$tag['tag_text']] = array('username' => htmlspecialchars($tag['tag_text']));
+			$results[$tag['tag_text']] = $this->_getPresentationForResult($tag);
 		}
 
 		return array('results' => $results);
+	}
+
+	protected function _getPresentationForResult($tag)
+	{
+		if (XenForo_Application::$versionId < 1020000)
+		{
+			return array('username' => htmlspecialchars($tag['tag_text']));
+		}
+		else
+		{
+			return htmlspecialchars($tag['tag_text']);
+		}
 	}
 
 }
