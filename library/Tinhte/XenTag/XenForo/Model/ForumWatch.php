43e32898d5028932817a79db80eb89a6bd57561e
Dao Hoang Son <daohoangson@gmail.com>
2014-04-07 10:09:18 +0700

    v2.0.0d -- fixed bug no email and alert update

--- library/Tinhte/XenTag/XenForo/Model/ForumWatch.php
+++ library/Tinhte/XenTag/XenForo/Model/ForumWatch.php
@@ -7,7 +7,20 @@ class Tinhte_XenTag_XenForo_Model_ForumWatch extends XFCP_Tinhte_XenTag_XenForo_
 		$result = parent::sendNotificationToWatchUsersOnMessage($post, $thread, $noAlerts, $noEmail);
 
 		Tinhte_XenTag_Integration::updateNoEmailAndAlert('post', $post['post_id'], $noEmail, $noAlerts);
-		Tinhte_XenTag_Integration::updateNoEmailAndAlert('post', $post['post_id'], $result['emailed'], $result['alerted']);
+
+		$emailed = array();
+		if (!empty($result['emailed']))
+		{
+			$emailed = $result['emailed'];
+		}
+
+		$alerted = array();
+		if (!empty($result['alerted']))
+		{
+			$alerted = $result['alerted'];
+		}
+
+		Tinhte_XenTag_Integration::updateNoEmailAndAlert('post', $post['post_id'], $emailed, $alerted);
 
 		return $result;
 	}
