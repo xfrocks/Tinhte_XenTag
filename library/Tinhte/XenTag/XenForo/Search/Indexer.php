ec9812fceb121e6dce7745f33c9bdb78547f8d3f
Dao Hoang Son <daohoangson@gmail.com>
2014-08-15 12:21:41 +0700

    improved indexer for better compatibility with other add-ons

--- library/Tinhte/XenTag/XenForo/Search/Indexer.php
+++ library/Tinhte/XenTag/XenForo/Search/Indexer.php
@@ -2,19 +2,20 @@
 
 class Tinhte_XenTag_XenForo_Search_Indexer extends XenForo_Search_Indexer
 {
+	protected $_otherIndexer = null;
 	protected $_extraMetadata = array();
 
 	public function __construct(XenForo_Search_Indexer $otherIndexer, array $extraMetadata)
 	{
 		$this->_sourceHandler = $otherIndexer->_sourceHandler;
+		$this->_otherIndexer = $otherIndexer;
 		$this->_extraMetadata = $extraMetadata;
 	}
 
 	public function insertIntoIndex($contentType, $contentId, $title, $message, $itemDate, $userId, $discussionId = 0, array $metadata = array())
 	{
 		$metadata = XenForo_Application::mapMerge($metadata, $this->_extraMetadata);
-
-		return parent::insertIntoIndex($contentType, $contentId, $title, $message, $itemDate, $userId, $discussionId, $metadata);
+		return $this->_otherIndexer->insertIntoIndex($contentType, $contentId, $title, $message, $itemDate, $userId, $discussionId, $metadata);
 	}
 
 }
