xfrocks/Tinhte_XenTag

commit c668cb50c6cc1760bcd484986bd18772d228737a

v2.1.3d -- added check for thread dw without forum data

library/Tinhte/XenTag/XenForo/DataWriter/Discussion/Thread.php
@@ -79,13 +79,16 @@ class Tinhte_XenTag_XenForo_DataWriter_Discussion_Thread_Base extends XFCP_Tinht
 			}
 
 			$forum = $this->Tinhte_XenTag_getForumData();
-			$options = Tinhte_XenTag_Helper::unserialize($forum[Tinhte_XenTag_Constants::FIELD_FORUM_OPTIONS]);
 			$requiresTag = Tinhte_XenTag_Option::get('requiresTag');
-			if (isset($options['requiresTag']) AND $options['requiresTag'] !== '')
+			$maximumTags = 0;
+			if (isset($forum[Tinhte_XenTag_Constants::FIELD_FORUM_OPTIONS]))
 			{
-				$requiresTag = $options['requiresTag'];
+				$options = Tinhte_XenTag_Helper::unserialize($forum[Tinhte_XenTag_Constants::FIELD_FORUM_OPTIONS]);
+				if (isset($options['requiresTag']) AND $options['requiresTag'] !== '') {
+					$requiresTag = $options['requiresTag'];
+				}
+				$maximumTags = intval($this->getModelFromCache('XenForo_Model_Forum')->Tinhte_XenTag_getMaximumTags($forum));
 			}
-			$maximumTags = intval($this->getModelFromCache('XenForo_Model_Forum')->Tinhte_XenTag_getMaximumTags($forum));
 
 			if ($requiresTag AND $maximumTags !== 0 AND $tagsCount == 0)
 			{

library/Tinhte/XenTag/addon-Tinhte_XenTag.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<addon addon_id="Tinhte_XenTag" title="[Tinhte] XenTag" version_string="2.1.3c" version_id="130" url="" install_callback_class="Tinhte_XenTag_Installer" install_callback_method="install" uninstall_callback_class="Tinhte_XenTag_Installer" uninstall_callback_method="uninstall">
+<addon addon_id="Tinhte_XenTag" title="[Tinhte] XenTag" version_string="2.1.3d" version_id="131" url="" install_callback_class="Tinhte_XenTag_Installer" install_callback_method="install" uninstall_callback_class="Tinhte_XenTag_Installer" uninstall_callback_method="uninstall">
   <admin_navigation>
     <navigation navigation_id="Tinhte_XenTag_Tag" parent_navigation_id="threadsPosts" display_order="9999" link="xentag-tags" admin_permission_id="Tinhte_XenTag" debug_only="0" hide_no_children="0"/>
   </admin_navigation>

commit b4aab8873d1537280ca67923a441c1ffc136eaf6

fixed bug maximum tags = 0 caused by c668cb5

library/Tinhte/XenTag/XenForo/DataWriter/Discussion/Thread.php
@@ -80,14 +80,13 @@ class Tinhte_XenTag_XenForo_DataWriter_Discussion_Thread_Base extends XFCP_Tinht
 
 			$forum = $this->Tinhte_XenTag_getForumData();
 			$requiresTag = Tinhte_XenTag_Option::get('requiresTag');
-			$maximumTags = 0;
+            $maximumTags = intval($this->getModelFromCache('XenForo_Model_Forum')->Tinhte_XenTag_getMaximumTags($forum));
 			if (isset($forum[Tinhte_XenTag_Constants::FIELD_FORUM_OPTIONS]))
 			{
 				$options = Tinhte_XenTag_Helper::unserialize($forum[Tinhte_XenTag_Constants::FIELD_FORUM_OPTIONS]);
 				if (isset($options['requiresTag']) AND $options['requiresTag'] !== '') {
 					$requiresTag = $options['requiresTag'];
 				}
-				$maximumTags = intval($this->getModelFromCache('XenForo_Model_Forum')->Tinhte_XenTag_getMaximumTags($forum));
 			}
 
 			if ($requiresTag AND $maximumTags !== 0 AND $tagsCount == 0)