1f7bdabfe8170f4c6c70fd72001a986ed13d6850
Dao Hoang Son <daohoangson@gmail.com>
2017-04-05 13:53:24 +0700

    Update coding convention

--- library/Tinhte/XenTag/Listener.php
+++ library/Tinhte/XenTag/Listener.php
@@ -2,46 +2,6 @@
 
 class Tinhte_XenTag_Listener
 {
-
-    public static function load_class($class, array &$extend)
-    {
-        static $classes = array(
-            'XenForo_ControllerAdmin_Forum',
-            'XenForo_ControllerAdmin_Page',
-            'XenForo_ControllerAdmin_Tag',
-            'XenForo_ControllerAdmin_Thread',
-            'XenForo_ControllerPublic_Tag',
-            'XenForo_ControllerPublic_Thread',
-            'XenForo_ControllerPublic_Watched',
-
-            'XenForo_DataWriter_Discussion_Thread',
-            'XenForo_DataWriter_DiscussionMessage_Post',
-            'XenForo_DataWriter_Forum',
-            'XenForo_DataWriter_Page',
-            'XenForo_DataWriter_Tag',
-
-            'XenForo_Deferred_ThreadAction',
-
-            'XenForo_Model_Forum',
-            'XenForo_Model_ForumWatch',
-            'XenForo_Model_Tag',
-            'XenForo_Model_Thread',
-
-            'XenForo_Search_DataHandler_Page',
-            'XenForo_Search_DataHandler_Post',
-
-            'XenForo_ViewPublic_Page_View',
-            'XenForo_ViewPublic_Tag_View',
-
-            // XenForo 1.5+
-            'XenForo_TagHandler_Tagger',
-        );
-
-        if (in_array($class, $classes)) {
-            $extend[] = 'Tinhte_XenTag_' . $class;
-        }
-    }
-
     public static function file_health_check(
         /** @noinspection PhpUnusedParameterInspection */
         XenForo_ControllerAdmin_Abstract $controller,
@@ -69,6 +29,8 @@ class Tinhte_XenTag_Listener
 
     public static function bb_code_hashtag(array $tag, array $rendererStates, XenForo_BbCode_Formatter_Base $formatter)
     {
+        // new XenForo_Phrase('custom_bb_code_hashtag_title')
+        // new XenForo_Phrase('custom_bb_code_hashtag_desc')
         $tagText = $formatter->stringifyTree($tag['children']);
         if (substr($tagText, 0, 1) === '#') {
             $tagText = substr($tagText, 1);
@@ -130,6 +92,160 @@ class Tinhte_XenTag_Listener
     {
         if ($class === 'XenForo_ViewPublic_Thread_ViewNewPosts') {
             $extend[] = 'Tinhte_XenTag_XenForo_ViewPublic_Thread_ViewNewPosts';
+        }
+    }
+
+    public static function load_class_XenForo_TagHandler_Tagger($class, array &$extend)
+    {
+        if ($class === 'XenForo_TagHandler_Tagger') {
+            $extend[] = 'Tinhte_XenTag_XenForo_TagHandler_Tagger';
+        }
+    }
+
+    public static function load_class_XenForo_Deferred_ThreadAction($class, array &$extend)
+    {
+        if ($class === 'XenForo_Deferred_ThreadAction') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Deferred_ThreadAction';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerAdmin_Forum($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerAdmin_Forum') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerAdmin_Forum';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerAdmin_Page($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerAdmin_Page') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerAdmin_Page';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerAdmin_Tag($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerAdmin_Tag') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerAdmin_Tag';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerAdmin_Thread($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerAdmin_Thread') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerAdmin_Thread';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerPublic_Tag($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerPublic_Tag') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerPublic_Tag';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerPublic_Thread($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerPublic_Thread') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerPublic_Thread';
+        }
+    }
+
+    public static function load_class_XenForo_ControllerPublic_Watched($class, array &$extend)
+    {
+        if ($class === 'XenForo_ControllerPublic_Watched') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ControllerPublic_Watched';
+        }
+    }
+
+    public static function load_class_XenForo_DataWriter_Discussion_Thread($class, array &$extend)
+    {
+        if ($class === 'XenForo_DataWriter_Discussion_Thread') {
+            $extend[] = 'Tinhte_XenTag_XenForo_DataWriter_Discussion_Thread';
+        }
+    }
+
+    public static function load_class_4f477c58235ffb475271e2521731d700($class, array &$extend)
+    {
+        if ($class === 'XenForo_DataWriter_DiscussionMessage_Post') {
+            $extend[] = 'Tinhte_XenTag_XenForo_DataWriter_DiscussionMessage_Post';
+        }
+    }
+
+    public static function load_class_XenForo_DataWriter_Forum($class, array &$extend)
+    {
+        if ($class === 'XenForo_DataWriter_Forum') {
+            $extend[] = 'Tinhte_XenTag_XenForo_DataWriter_Forum';
+        }
+    }
+
+    public static function load_class_XenForo_DataWriter_Page($class, array &$extend)
+    {
+        if ($class === 'XenForo_DataWriter_Page') {
+            $extend[] = 'Tinhte_XenTag_XenForo_DataWriter_Page';
+        }
+    }
+
+    public static function load_class_XenForo_DataWriter_Tag($class, array &$extend)
+    {
+        if ($class === 'XenForo_DataWriter_Tag') {
+            $extend[] = 'Tinhte_XenTag_XenForo_DataWriter_Tag';
+        }
+    }
+
+    public static function load_class_XenForo_Model_Forum($class, array &$extend)
+    {
+        if ($class === 'XenForo_Model_Forum') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Model_Forum';
+        }
+    }
+
+    public static function load_class_XenForo_Model_ForumWatch($class, array &$extend)
+    {
+        if ($class === 'XenForo_Model_ForumWatch') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Model_ForumWatch';
+        }
+    }
+
+    public static function load_class_XenForo_Model_Tag($class, array &$extend)
+    {
+        if ($class === 'XenForo_Model_Tag') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Model_Tag';
+        }
+    }
+
+    public static function load_class_XenForo_Model_Thread($class, array &$extend)
+    {
+        if ($class === 'XenForo_Model_Thread') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Model_Thread';
+        }
+    }
+
+    public static function load_class_XenForo_Search_DataHandler_Page($class, array &$extend)
+    {
+        if ($class === 'XenForo_Search_DataHandler_Page') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Search_DataHandler_Page';
+        }
+    }
+
+    public static function load_class_XenForo_Search_DataHandler_Post($class, array &$extend)
+    {
+        if ($class === 'XenForo_Search_DataHandler_Post') {
+            $extend[] = 'Tinhte_XenTag_XenForo_Search_DataHandler_Post';
+        }
+    }
+
+    public static function load_class_XenForo_ViewPublic_Page_View($class, array &$extend)
+    {
+        if ($class === 'XenForo_ViewPublic_Page_View') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ViewPublic_Page_View';
+        }
+    }
+
+    public static function load_class_XenForo_ViewPublic_Tag_View($class, array &$extend)
+    {
+        if ($class === 'XenForo_ViewPublic_Tag_View') {
+            $extend[] = 'Tinhte_XenTag_XenForo_ViewPublic_Tag_View';
         }
     }
 }
