bb06009893ee74bdb9af5b34501c7e60d7c68ea5
Luu Truong <truonglv@outlook.com>
2018-04-12 14:43:33 +0700

    Index first post with thread tags

--- library/Tinhte/XenTag/XenForo/Search/DataHandler/Post.php
+++ library/Tinhte/XenTag/XenForo/Search/DataHandler/Post.php
@@ -6,10 +6,25 @@ class Tinhte_XenTag_XenForo_Search_DataHandler_Post extends XFCP_Tinhte_XenTag_X
     {
         $metadata = array();
 
+        if (!isset($data[Tinhte_XenTag_Constants::FIELD_POST_TAGS])
+            && Tinhte_XenTag_Option::get('indexFirstPostTags')
+        ) {
+            if ($parentData
+                && ($parentData['first_post_id'] == $data['post_id'] || $parentData['first_post_id'] === 0)
+                && $data['message_state'] == 'visible'
+                && $parentData['discussion_state'] == 'visible'
+            ) {
+                $data[Tinhte_XenTag_Constants::FIELD_POST_TAGS] = $parentData['tags'];
+            }
+        }
+
         if (isset($data[Tinhte_XenTag_Constants::FIELD_POST_TAGS])) {
             $title = '';
             Tinhte_XenTag_Helper::prepareSearchIndex($title, $metadata,
                 $data[Tinhte_XenTag_Constants::FIELD_POST_TAGS]);
+        }
+
+        if (!empty($metadata)) {
             $indexer = new Tinhte_XenTag_XenForo_Search_Indexer($indexer, $metadata);
         }
 
